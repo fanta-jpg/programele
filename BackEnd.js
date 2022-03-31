8da8bf1e3295c1a263257200481f853b758f5925
Cartelis <sap.deividas@gmail.com>
2022-03-31 23:31:10 +0300

    Kelia varda tvarkarasti su dienu pavadinimais vienam masyve

--- BackEnd.js
+++ BackEnd.js
@@ -10,8 +10,8 @@ async function scrapeInfo(url) {
 
     var pirmas = new Boolean(true);
 
-    for (let y=2; y<139; y++) {
-        for (let x=1; x<4; x++) {
+    for (let y=117; y<118; y++) {
+        for (let x=1; x<3; x++) {
             try {
                 const [el] = await page.$x('/html/body/font/center[2]/font/center/b/table/tbody/tr[' + y + ']/td[' + x + ']/a');
                 const href = await el.getProperty('href');
@@ -23,33 +23,124 @@ async function scrapeInfo(url) {
 
                 //console.log({link, title});
 
+
                 if (pirmas == false) fs.appendFileSync('Duomenys.json', ',', "UTF-8",{'flags': 'a+'});
-                fs.appendFileSync("Duomenys.json", JSON.stringify({link, title},null,2), "UTF-8",{'flags': 'a+'});
-                var pirmas = false;
+                pirmas = false;
+
+                // No. 1
+                //fs.appendFileSync("Duomenys.json", JSON.stringify({title},null,2), "UTF-8",{'flags': 'a+'});
+                //scrapeSchedule(link);
+
+                let schedule = [title];
+
+
+                await page.goto(link);
+
+                for (let m=2; m<=6; m++) {
+                    switch (m) {
+                    case 2: 
+                        schedule.push('Pirmadienis:');
+                        // fs.appendFileSync("Duomenys.json", 'Pirmadienis:', "UTF-8",{'flags': 'a+'});
+                        break;
+                    case 3: 
+                        schedule.push('Antradienis:');
+                        // fs.appendFileSync("Duomenys.json", 'Antradienis:', "UTF-8",{'flags': 'a+'});
+                        break;
+                    case 4:
+                        schedule.push('Trečiadienis:');
+                        // fs.appendFileSync("Duomenys.json", 'Trečiadienis:', "UTF-8",{'flags': 'a+'});
+                        break;
+                    case 5:
+                        schedule.push('Ketvirtadienis:');
+                        // fs.appendFileSync("Duomenys.json", 'Ketvirtadienis:', "UTF-8",{'flags': 'a+'});
+                        break;
+                    case 6:
+                        schedule.push('Penktadienis:');
+                        // fs.appendFileSync("Duomenys.json", 'Penktadienis:', "UTF-8",{'flags': 'a+'});
+                        break;
+                    }
+
+                    
+                    for (let n=3; n<=10; n++) {
+                        try {
+                            const [elp] = await page.$x('/html/body/font/center[2]/table/tbody/tr[' + n + ']/td[' + m + ']/font/b/a');
+                            const txt2 = await elp.getProperty('textContent');
+                            const lesson = await txt2.jsonValue();
+                            
+                            schedule.push(lesson);
+
+                           // fs.appendFileSync("Duomenys.json", JSON.stringify(lesson,null,2), "UTF-8",{'flags': 'a+'});
+                        } catch (e) {}
+                    }
+                }
+
+                fs.appendFileSync("Duomenys.json", JSON.stringify({schedule},null,2), "UTF-8",{'flags': 'a+'});
+
+                //fs.appendFileSync("Duomenys.json", '\n', "UTF-8",{'flags': 'a+'});
+                //var pirmas = false;
+
+                await page.goto('http://www.azuolynogimnazija.lt/uploads/tvark/tvark2022_2/index.htm');
                 
             } catch (e) {}
         }
     }
+    
     fs.appendFileSync('Duomenys.json', ']', "UTF-8",{'flags': 'a+'});
 
     browser.close();
 }
 
 async function scrapeSchedule(url) {
-    const browser = await puppeteer.launch();
+    //const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
 
     for (let m=2; m<=6; m++) {
+        switch (m) {
+        case 2: 
+            fs.appendFileSync("Duomenys.json", 'Pirmadienis:', "UTF-8",{'flags': 'a+'});
+            break;
+        case 3: 
+            fs.appendFileSync("Duomenys.json", 'Antradienis:', "UTF-8",{'flags': 'a+'});
+            break;
+        case 4:
+            fs.appendFileSync("Duomenys.json", 'Trečiadienis:', "UTF-8",{'flags': 'a+'});
+            break;
+        case 5:
+            fs.appendFileSync("Duomenys.json", 'Ketvirtadienis:', "UTF-8",{'flags': 'a+'});
+            break;
+        case 6:
+            fs.appendFileSync("Duomenys.json", 'Penktadienis:', "UTF-8",{'flags': 'a+'});
+            break;
+        }
+        
         for (let n=3; n<=10; n++) {
             try {
                 const [elp] = await page.$x('/html/body/font/center[2]/table/tbody/tr[' + n + ']/td[' + m + ']/font/b/a');
                 const txt2 = await elp.getProperty('textContent');
                 const lesson = await txt2.jsonValue();
 
-                console.log({lesson});
+                // if (m==2) {
+                //     fs.appendFileSync("Duomenys.json", 'Pirmadienis:', "UTF-8",{'flags': 'a+'});
+                // }
+                // if (m==3) {
+                //     fs.appendFileSync("Duomenys.json", 'Antradienis:', "UTF-8",{'flags': 'a+'});
+                // }
+                // if (m==4) {
+                //     fs.appendFileSync("Duomenys.json", 'Trečiadienis:', "UTF-8",{'flags': 'a+'});
+                // }
+                // if (m==5) {
+                //     fs.appendFileSync("Duomenys.json", 'Ketvirtadienis:', "UTF-8",{'flags': 'a+'});
+                // }
+                // if (m==6) {
+                //     fs.appendFileSync("Duomenys.json", 'Penktadienis:', "UTF-8",{'flags': 'a+'});
+                // }
+
+                fs.appendFileSync("Duomenys.json", JSON.stringify({lesson},null,2), "UTF-8",{'flags': 'a+'});
+                //return Promise.resolve(lesson);
+
             } catch (e) {
-                console.log('');
+                //console.log('');
             }
         }
     }
@@ -58,6 +149,37 @@ async function scrapeSchedule(url) {
 }
 
 
-//scrapeInfo ('http://www.azuolynogimnazija.lt/uploads/tvark/tvark2022_2/index.htm');
+scrapeInfo ('http://www.azuolynogimnazija.lt/uploads/tvark/tvark2022_2/index.htm');
 
-scrapeSchedule ('http://www.azuolynogimnazija.lt/uploads/tvark/tvark2022_2/x300123c_sapd.htm');
+
+
+// No. 1
+                // await page.goto(link);
+
+                // for (let m=2; m<=6; m++) {
+                //     switch (m) {
+                //     case 2: 
+                //         fs.appendFileSync("Duomenys.json", 'Pirmadienis:', "UTF-8",{'flags': 'a+'});
+                //         break;
+                //     case 3: 
+                //         fs.appendFileSync("Duomenys.json", 'Antradienis:', "UTF-8",{'flags': 'a+'});
+                //         break;
+                //     case 4:
+                //         fs.appendFileSync("Duomenys.json", 'Trečiadienis:', "UTF-8",{'flags': 'a+'});
+                //         break;
+                //     case 5:
+                //         fs.appendFileSync("Duomenys.json", 'Ketvirtadienis:', "UTF-8",{'flags': 'a+'});
+                //         break;
+                //     case 6:
+                //         fs.appendFileSync("Duomenys.json", 'Penktadienis:', "UTF-8",{'flags': 'a+'});
+                //         break;
+                //     }
+
+                //     for (let n=3; n<=10; n++) {
+                //         try {
+                //             const [elp] = await page.$x('/html/body/font/center[2]/table/tbody/tr[' + n + ']/td[' + m + ']/font/b/a');
+                //             const txt2 = await elp.getProperty('textContent');
+                //             const lesson = await txt2.jsonValue();
+                //         } catch (e) {}
+                //     }
+                // }
